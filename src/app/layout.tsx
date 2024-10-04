ac8fd744d2307897ce4af20aa27fd30cdfd559c2
Ace G <aceguevarra48@gmail.com>
2024-10-04 23:12:33 +0800

    fix: insert correct favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/blackjack logo.png" />
+      </head>
       <body
         className={`${figtree.className} antialiased bg-[#055814] overflow-hidden`}
       >
